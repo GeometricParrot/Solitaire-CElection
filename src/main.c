f80b73c6aa7fc82f84d9a1ad12fa4931c134bf6e
GeometricParrot <98898757+GeometricParrot@users.noreply.github.com>
2025-12-13 16:25:29 -0600

    basic rendering

--- src/main.c
+++ src/main.c
@@ -7,30 +7,82 @@
 
 #include <string.h>
 
-#define CARD_FACEUP 1 << 6
-#define CARD_FACEDOWN 0
+#define CARD_SUIT_MASK 0b11
+#define CARD_CLUBS 3
+#define CARD_DIAMONDS 2
+#define CARD_HEARTS 1
+#define CARD_SPADES 0
+#define CARD_VALUE_MASK 0b1111 << 2
+#define CARD_A 0
+#define CARD_2 1
+#define CARD_3 2
+#define CARD_4 3
+#define CARD_5 4
+#define CARD_6 5
+#define CARD_7 6
+#define CARD_8 7
+#define CARD_9 8
+#define CARD_10 9
+#define CARD_J 10
+#define CARD_Q 11
+#define CARD_K 12
 
-#define CARD_SUIT_CLUBS 0b11 << 4
-#define CARD_SUIT_DIAMONDS 0b10 << 4
-#define CARD_SUIT_HEARTS 0b01 << 4
-#define CARD_SUIT_SPADES 0b0 << 4
-#define CARD_INVALID 255
 
-#define CARD_ACE 1
-#define CARD_2 2
-#define CARD_3 3
-#define CARD_4 4
-#define CARD_5 5
-#define CARD_6 6
-#define CARD_7 7
-#define CARD_8 8
-#define CARD_9 9
-#define CARD_10 10
-#define CARD_JACK 11
-#define CARD_QUEEN 12
-#define CARD_KING 13
-#define CARD_JOKER 14
-
+gfx_sprite_t** glifs_flipped;
+gfx_sprite_t* glifs[] = {
+	red_a,
+	red_2,
+	red_3,
+	red_4,
+	red_5,
+	red_6,
+	red_7,
+	red_8,
+	red_9,
+	red_10,
+	red_j,
+	red_q,
+	red_k,
+	orange_a,
+	orange_2,
+	orange_3,
+	orange_4,
+	orange_5,
+	orange_6,
+	orange_7,
+	orange_8,
+	orange_9,
+	orange_10,
+	orange_j,
+	orange_q,
+	orange_k,
+	blue_a,
+	blue_2,
+	blue_3,
+	blue_4,
+	blue_5,
+	blue_6,
+	blue_7,
+	blue_8,
+	blue_9,
+	blue_10,
+	blue_j,
+	blue_q,
+	blue_k,
+	green_a,
+	green_2,
+	green_3,
+	green_4,
+	green_5,
+	green_6,
+	green_7,
+	green_8,
+	green_9,
+	green_10,
+	green_j,
+	green_q,
+	green_k,
+};
 
 typedef uint8_t u8;
 typedef int8_t i8;
@@ -54,55 +106,9 @@ typedef struct {
 int step(State* state);
 void draw(State* state);
 
-gfx_sprite_t* red_a_flipped;
-gfx_sprite_t* red_2_flipped;
-gfx_sprite_t* red_3_flipped;
-gfx_sprite_t* red_4_flipped;
-gfx_sprite_t* red_5_flipped;
-gfx_sprite_t* red_6_flipped;
-gfx_sprite_t* red_7_flipped;
-gfx_sprite_t* red_8_flipped;
-gfx_sprite_t* red_9_flipped;
-gfx_sprite_t* red_10_flipped;
-gfx_sprite_t* red_j_flipped;
-gfx_sprite_t* red_q_flipped;
-gfx_sprite_t* red_k_flipped;
+	//glif_sprites = malloc(sizeof(gfx_rletsprite_t) * 13 * 4 * 2);
+	//glif_sprites[GLIF_A | GLIF_CLUBS] = gfx_ConvertMallocRLETSprite(red_a);
 
-
-int copy_sprites_to_rotated() {
-	red_a_flipped = gfx_RotateSpriteHalf(red_a, gfx_MallocSprite(6, 10));
-	red_2_flipped = gfx_RotateSpriteHalf(red_2, gfx_MallocSprite(6, 10));
-	red_3_flipped = gfx_RotateSpriteHalf(red_3, gfx_MallocSprite(6, 10));
-	red_4_flipped = gfx_RotateSpriteHalf(red_4, gfx_MallocSprite(6, 10));
-	red_5_flipped = gfx_RotateSpriteHalf(red_5, gfx_MallocSprite(6, 10));
-	red_6_flipped = gfx_RotateSpriteHalf(red_6, gfx_MallocSprite(6, 10));
-	red_7_flipped = gfx_RotateSpriteHalf(red_7, gfx_MallocSprite(6, 10));
-	red_8_flipped = gfx_RotateSpriteHalf(red_8, gfx_MallocSprite(6, 10));
-	red_9_flipped = gfx_RotateSpriteHalf(red_9, gfx_MallocSprite(6, 10));
-	red_10_flipped=gfx_RotateSpriteHalf(red_10, gfx_MallocSprite(8, 10));
-	red_j_flipped = gfx_RotateSpriteHalf(red_j, gfx_MallocSprite(6, 10));
-	red_q_flipped = gfx_RotateSpriteHalf(red_q, gfx_MallocSprite(7, 10));
-	red_k_flipped = gfx_RotateSpriteHalf(red_k, gfx_MallocSprite(6, 10));
-
-	return 0;
-}
-
-int free_sprites_rotated() {
-	free(red_a_flipped);
-	free(red_2_flipped);
-	free(red_3_flipped);
-	free(red_4_flipped);
-	free(red_5_flipped);
-	free(red_6_flipped);
-	free(red_7_flipped);
-	free(red_8_flipped);
-	free(red_9_flipped);
-	free(red_10_flipped);
-	free(red_j_flipped);
-	free(red_q_flipped);
-	free(red_k_flipped);
-	return 0;
-}
 
 u24 terriblerand() {
 	static u24 state = 0b100011011110111111001100;
@@ -136,7 +142,7 @@ int CardStorage_add_card(CardStorage* cs, Card new_card) {
 
 Card CardStorage_draw_card(CardStorage* cs, u8 index) {
 	if (index >= cs->usage)
-		return CARD_INVALID;
+		return -1;
 	Card out = cs->data[index];
 	memmove(&cs->data[index], &cs->data[index + 1], sizeof(Card) * ((cs->usage - 1) - index));
 	--cs->usage;
@@ -162,10 +168,71 @@ void CardStorage_debug_print(CardStorage* cs) {
 	for (u8 i = 0; i < cs->usage; ++i) {
 		dbg_printf("card: %d\n", (u24)cs->data[i] & 0b00001111);
 	}
+	//	dbg_printf("card value: %d, card suit: %d, full card: %d\n", ((u24)card & 0b111100) >> 2, (u24)card & 0b11, (u24)card);
 }
 
 int main(void)
 {
+	gfx_sprite_t* temp1[] = {
+		gfx_RotateSpriteHalf(red_a, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_2, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_3, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_4, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_5, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_6, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_7, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_8, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_9, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_10, gfx_MallocSprite(8, 10)),
+		gfx_RotateSpriteHalf(red_j, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(red_q, gfx_MallocSprite(7, 10)),
+		gfx_RotateSpriteHalf(red_k, gfx_MallocSprite(6, 10)),
+
+		gfx_RotateSpriteHalf(orange_a, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_2, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_3, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_4, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_5, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_6, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_7, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_8, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_9, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_10, gfx_MallocSprite(8, 10)),
+		gfx_RotateSpriteHalf(orange_j, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(orange_q, gfx_MallocSprite(7, 10)),
+		gfx_RotateSpriteHalf(orange_k, gfx_MallocSprite(6, 10)),
+
+		gfx_RotateSpriteHalf(blue_a, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_2, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_3, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_4, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_5, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_6, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_7, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_8, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_9, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_10, gfx_MallocSprite(8, 10)),
+		gfx_RotateSpriteHalf(blue_j, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(blue_q, gfx_MallocSprite(7, 10)),
+		gfx_RotateSpriteHalf(blue_k, gfx_MallocSprite(6, 10)),
+
+		gfx_RotateSpriteHalf(green_a, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_2, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_3, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_4, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_5, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_6, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_7, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_8, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_9, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_10, gfx_MallocSprite(8, 10)),
+		gfx_RotateSpriteHalf(green_j, gfx_MallocSprite(6, 10)),
+		gfx_RotateSpriteHalf(green_q, gfx_MallocSprite(7, 10)),
+		gfx_RotateSpriteHalf(green_k, gfx_MallocSprite(6, 10)),
+	};
+
+	glifs_flipped = temp1;
+
 	State state = {
 		0,
 		{0, 0, 0},
@@ -174,27 +241,18 @@ int main(void)
 	};
 
 	gfx_Begin();
-	copy_sprites_to_rotated();
 	gfx_SetPalette(global_palette, sizeof_global_palette, 0);
+	gfx_SetTransparentColor(1);
     gfx_SetDrawBuffer();
 
 	CardStorage_resize(&state.playfield, 52);
 	CardStorage_resize(&state.deck, 52);
 	for (u8 i = 0; i < 52; ++i) {
-		Card generated_card = CARD_FACEUP;
-		if (i < 13)
-			generated_card |= CARD_SUIT_SPADES;
-		else if (i < 26)
-			generated_card |= CARD_SUIT_HEARTS;
-		else if (i < 39)
-			generated_card |= CARD_SUIT_DIAMONDS;
-		else
-			generated_card |= CARD_SUIT_CLUBS;
-		generated_card |= (i % 13) + 1;
+		Card generated_card = i;
 		CardStorage_add_card(&state.deck, generated_card); 
 	}
 
-	CardStorage_shuffle(&state.deck);
+	//CardStorage_shuffle(&state.deck);
 	CardStorage_debug_print(&state.deck);
 
     while (step(&state))
@@ -202,7 +260,6 @@ int main(void)
         draw(&state);
         gfx_SwapDraw();
     }
-	free_sprites_rotated();
     gfx_End();
 
     return 0;
@@ -211,8 +268,8 @@ int main(void)
 // main looping logic
 int step(State* state)
 {
-	if (state->playfield.usage < 14) {
-		Card drawncard = CardStorage_draw_card(&state->deck, state->deck.usage - 1);
+	if (state->playfield.usage < 28) {
+		Card drawncard = CardStorage_draw_card(&state->deck, 0);
 		CardStorage_add_card(&state->playfield, drawncard);
 	}
 	(state->uptime) += 1;
@@ -221,75 +278,24 @@ int step(State* state)
 
 int drawCard(Card card, int x, int y) {
 	gfx_TransparentSprite(blank_card, x, y);
-	// flipped because sprite flipping is weird
-	int glif1_x = x + (40 - 2 - 6);
-	int glif1_y = y + (56 - 10 - 2);
-	int glif2_x = x + 2;
-	int glif2_y = y + 2;
-	switch (card & 0b00001111) {
-		case CARD_ACE:
-			gfx_TransparentSprite(red_a, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_a_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_2:
-			gfx_TransparentSprite(red_2, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_2_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_3:
-			gfx_TransparentSprite(red_3, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_3_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_4:
-			gfx_TransparentSprite(red_4, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_4_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_5:
-			gfx_TransparentSprite(red_5, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_5_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_6:
-			gfx_TransparentSprite(red_6, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_6_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_7:
-			gfx_TransparentSprite(red_7, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_7_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_8:
-			gfx_TransparentSprite(red_8, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_8_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_9:
-			gfx_TransparentSprite(red_9, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_9_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_10:
-			gfx_TransparentSprite(red_10, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_10_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_JACK:
-			gfx_TransparentSprite(red_j, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_j_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_QUEEN:
-			gfx_TransparentSprite(red_q, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_q_flipped, glif2_x, glif2_y);
-			break;
-		case CARD_KING:
-			gfx_TransparentSprite(red_k, glif1_x, glif1_y);
-			gfx_TransparentSprite(red_k_flipped, glif2_x, glif2_y);
-			break;
-		default:
-			dbg_printf("card: %d\n", (u24)card & 0b00001111);
-			return 1;
-	}
+	int glif1_x = x + 2;
+	int glif1_y = y + 2;
+	int glif2_x = x + 32; // 40 - 2 - 6
+	int glif2_y = y + 44; // 56 - 10 - 2
+	if ((card & CARD_VALUE_MASK) == CARD_10)
+		glif2_x -= 2;
+	if ((card & CARD_VALUE_MASK) == CARD_Q)
+		glif2_x -= 1;
+	gfx_TransparentSprite(glifs[card], glif1_x, glif1_y);
+	gfx_TransparentSprite(glifs_flipped[card], glif2_x, glif2_y);
+	dbg_printf("card value: %d, card suit: %d, full card: %d\n", ((u24)card & CARD_VALUE_MASK) >> 2, (u24)card & 0b11, (u24)card);
 	return 0;
 }
 
 // draw graphics
 void draw(State* state)
 {
-	gfx_FillScreen(1);
+	gfx_ZeroScreen();
 	for (u8 i = 0; i < state->playfield.usage; ++i) {
 		drawCard(state->playfield.data[i], (i % 7) * 46, (i / 7) * 58);
 	}
