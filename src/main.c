145cb2267c4e74d484505e3d9d252175ce3664e7
GeometricParrot <98898757+GeometricParrot@users.noreply.github.com>
2025-12-13 19:43:55 -0600

    Pregame

--- src/main.c
+++ src/main.c
@@ -13,19 +13,22 @@
 #define CARD_HEARTS 1
 #define CARD_SPADES 0
 #define CARD_VALUE_MASK 0b1111 << 2
-#define CARD_A 0
-#define CARD_2 1
-#define CARD_3 2
-#define CARD_4 3
-#define CARD_5 4
-#define CARD_6 5
-#define CARD_7 6
-#define CARD_8 7
-#define CARD_9 8
-#define CARD_10 9
-#define CARD_J 10
-#define CARD_Q 11
-#define CARD_K 12
+#define CARD_A 0 << 2
+#define CARD_2 1 << 2
+#define CARD_3 2 << 2
+#define CARD_4 3 << 2
+#define CARD_5 4 << 2
+#define CARD_6 5 << 2
+#define CARD_7 6 << 2
+#define CARD_8 7 << 2
+#define CARD_9 8 << 2
+#define CARD_10 9 << 2
+#define CARD_J 10 << 2
+#define CARD_Q 11 << 2
+#define CARD_K 12 << 2
+#define CARD_FACING_MASK 1 << 6
+#define CARD_FACEUP 0
+#define CARD_FACEDOWN 1 << 6
 
 
 gfx_sprite_t** glifs_flipped;
@@ -99,12 +102,14 @@ typedef struct {
 typedef struct {
 	u24 uptime;
 	CardStorage deck;
-	CardStorage playfield;
+	CardStorage discard;
+	CardStorage colums[7];
 	CardStorage scoring;
 } State;
 
 int step(State* state);
 void draw(State* state);
+int init_gamestate(State* state);	
 
 	//glif_sprites = malloc(sizeof(gfx_rletsprite_t) * 13 * 4 * 2);
 	//glif_sprites[GLIF_A | GLIF_CLUBS] = gfx_ConvertMallocRLETSprite(red_a);
@@ -166,7 +171,7 @@ void CardStorage_debug_print(CardStorage* cs) {
 	dbg_printf("starage capacity: %d\n", (u24)cs->capacity);
 	dbg_printf("starage usage: %d\n", (u24)cs->usage);
 	for (u8 i = 0; i < cs->usage; ++i) {
-		dbg_printf("card: %d\n", (u24)cs->data[i] & 0b00001111);
+		dbg_printf("card: %d\n", (u24)cs->data[i]);
 	}
 	//	dbg_printf("card value: %d, card suit: %d, full card: %d\n", ((u24)card & 0b111100) >> 2, (u24)card & 0b11, (u24)card);
 }
@@ -230,30 +235,28 @@ int main(void)
 		gfx_RotateSpriteHalf(green_q, gfx_MallocSprite(7, 10)),
 		gfx_RotateSpriteHalf(green_k, gfx_MallocSprite(6, 10)),
 	};
-
 	glifs_flipped = temp1;
 
 	State state = {
 		0,
 		{0, 0, 0},
 		{0, 0, 0},
+		{
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+		},
 		{0, 0, 0},
 	};
+	init_gamestate(&state);
 
 	gfx_Begin();
 	gfx_SetPalette(global_palette, sizeof_global_palette, 0);
 	gfx_SetTransparentColor(1);
     gfx_SetDrawBuffer();
-
-	CardStorage_resize(&state.playfield, 52);
-	CardStorage_resize(&state.deck, 52);
-	for (u8 i = 0; i < 52; ++i) {
-		Card generated_card = i;
-		CardStorage_add_card(&state.deck, generated_card); 
-	}
-
-	//CardStorage_shuffle(&state.deck);
-	CardStorage_debug_print(&state.deck);
 
     while (step(&state))
     {
@@ -265,18 +268,48 @@ int main(void)
     return 0;
 }
 
+int init_gamestate(State* state) {
+	dbg_printf("initing\n");
+	// populate the deck with one of each card
+	CardStorage_resize(&state->deck, 52);
+	for (u8 i = 0; i < 52; ++i) {
+		CardStorage_add_card(&state->deck, i); 
+	}
+	CardStorage_shuffle(&state->deck);
+	CardStorage_debug_print(&state->deck);
+
+	// initial deal
+	for (u8 i = 0; i < 7; ++i) {
+		CardStorage_resize(&state->colums[i], 15);
+	}
+	for (u8 i = 0; i < 7; ++i) {
+		for (u8 j = i; j < 7; ++j) {
+			Card card = CardStorage_draw_card(&state->deck, state->deck.usage - 1);
+			if (i == j) {
+				card |= CARD_FACEUP;
+			}
+			else {
+				card |= CARD_FACEDOWN;
+			}
+			CardStorage_add_card(&state->colums[j], card);
+		}
+	}
+
+	return 0;
+}
+
 // main looping logic
 int step(State* state)
 {
-	if (state->playfield.usage < 28) {
-		Card drawncard = CardStorage_draw_card(&state->deck, 0);
-		CardStorage_add_card(&state->playfield, drawncard);
-	}
 	(state->uptime) += 1;
     return !os_GetCSC();
 }
 
 int drawCard(Card card, int x, int y) {
+	if ((card & CARD_FACING_MASK) == CARD_FACEDOWN) {
+		gfx_TransparentSprite(bard_back, x, y);
+		return 0;
+	}
 	gfx_TransparentSprite(blank_card, x, y);
 	int glif1_x = x + 2;
 	int glif1_y = y + 2;
@@ -296,8 +329,10 @@ int drawCard(Card card, int x, int y) {
 void draw(State* state)
 {
 	gfx_ZeroScreen();
-	for (u8 i = 0; i < state->playfield.usage; ++i) {
-		drawCard(state->playfield.data[i], (i % 7) * 46, (i / 7) * 58);
+	for (u8 colum = 0; colum < 7; ++colum) {
+		for (u8 j = 0; j < state->colums[colum].usage; ++j) {
+			drawCard(state->colums[colum].data[j], colum * 46, j * 17 + 60);
+		}
 	}
 }
 
