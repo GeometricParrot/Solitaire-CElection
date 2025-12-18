001403e91b6303db01c67b696760d9d661b192ea
GeometricParrot <98898757+GeometricParrot@users.noreply.github.com>
2025-12-18 12:05:02 -0600

    Cards can be moved

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include <ti/getcsc.h>
 #include <graphx.h>
+#include <keypadc.h>
 
 #include "gfx/gfx.h"
 
@@ -30,6 +31,18 @@
 #define CARD_FACEUP 0
 #define CARD_FACEDOWN 1 << 6
 
+#define DECK 0
+#define DISCARD 1
+#define COLUM 2
+#define SCORING 9
+
+#define GAME_STATE_SELECT_SOURCE = 0
+#define GAME_STATE_SELECT_TARGET = 1
+
+
+#define min(a,b) (((a) < (b)) ? (a) : (b))
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#define bound(a, b, c) (max(b, min(a, c)))
 
 gfx_sprite_t** glifs_flipped;
 gfx_sprite_t* glifs[] = {
@@ -100,11 +113,12 @@ typedef struct {
 } CardStorage;
 
 typedef struct {
+	u8 gamestate;
+	u8 source_storage;
+	u8 source_index;
+	u8 target_storage;
 	u24 uptime;
-	CardStorage deck;
-	CardStorage discard;
-	CardStorage colums[7];
-	CardStorage scoring;
+	CardStorage storages[13];
 } State;
 
 int step(State* state);
@@ -145,13 +159,19 @@ int CardStorage_add_card(CardStorage* cs, Card new_card) {
 	return 0;
 }
 
-Card CardStorage_draw_card(CardStorage* cs, u8 index) {
+Card CardStorage_take_card(CardStorage* cs, u8 index) {
 	if (index >= cs->usage)
 		return -1;
 	Card out = cs->data[index];
 	memmove(&cs->data[index], &cs->data[index + 1], sizeof(Card) * ((cs->usage - 1) - index));
 	--cs->usage;
 	return out;
+}
+
+Card CardStorage_take_top_card(CardStorage* cs) {
+	if (cs->usage == 0)
+		return -1;
+	return cs->data[--cs->usage];
 }
 
 int CardStorage_shuffle(CardStorage* cs) {
@@ -238,10 +258,18 @@ int main(void)
 	glifs_flipped = temp1;
 
 	State state = {
-		0,
-		{0, 0, 0},
-		{0, 0, 0},
-		{
+		0, // gamestate
+		0, // source_storage
+		0, // source_index
+		0, // target_storage
+		0, // uptime
+		{ // storages
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
+			{0, 0, 0},
 			{0, 0, 0},
 			{0, 0, 0},
 			{0, 0, 0},
@@ -249,16 +277,17 @@ int main(void)
 			{0, 0, 0},
 			{0, 0, 0},
 		},
-		{0, 0, 0},
 	};
 	init_gamestate(&state);
 
 	gfx_Begin();
 	gfx_SetPalette(global_palette, sizeof_global_palette, 0);
 	gfx_SetTransparentColor(1);
+	gfx_SetTextFGColor(2);
+    gfx_SetTextBGColor(3);
     gfx_SetDrawBuffer();
 
-    while (step(&state))
+    while (step(&state) != sk_Clear)
     {
         draw(&state);
         gfx_SwapDraw();
@@ -271,27 +300,28 @@ int main(void)
 int init_gamestate(State* state) {
 	dbg_printf("initing\n");
 	// populate the deck with one of each card
-	CardStorage_resize(&state->deck, 52);
+	CardStorage_resize(&state->storages[DECK], 52);
+	CardStorage_resize(&state->storages[DISCARD], 52);
 	for (u8 i = 0; i < 52; ++i) {
-		CardStorage_add_card(&state->deck, i); 
+		CardStorage_add_card(&state->storages[DECK], i | CARD_FACEDOWN); 
 	}
-	CardStorage_shuffle(&state->deck);
-	CardStorage_debug_print(&state->deck);
+	CardStorage_shuffle(&state->storages[DECK]);
+	CardStorage_debug_print(&state->storages[DECK]);
 
 	// initial deal
 	for (u8 i = 0; i < 7; ++i) {
-		CardStorage_resize(&state->colums[i], 15);
+		CardStorage_resize(&state->storages[COLUM + i], 15);
 	}
 	for (u8 i = 0; i < 7; ++i) {
 		for (u8 j = i; j < 7; ++j) {
-			Card card = CardStorage_draw_card(&state->deck, state->deck.usage - 1);
+			Card card = CardStorage_take_top_card(&state->storages[DECK]);
 			if (i == j) {
-				card |= CARD_FACEUP;
+				card ^= CARD_FACEDOWN;
 			}
 			else {
 				card |= CARD_FACEDOWN;
 			}
-			CardStorage_add_card(&state->colums[j], card);
+			CardStorage_add_card(&state->storages[COLUM + j], card);
 		}
 	}
 
@@ -302,26 +332,91 @@ int init_gamestate(State* state) {
 int step(State* state)
 {
 	(state->uptime) += 1;
-    return !os_GetCSC();
+
+	uint8_t key;
+	key = os_GetCSC();
+
+	if (state->gamestate == 0) {
+		switch (key) {
+			case sk_Right:
+			if (state->source_storage < 12) {
+				++state->source_storage;
+				state->source_index = state->storages[state->source_storage].usage - 1;
+			}
+			break;
+			case sk_Left:
+			if (state->source_storage > 0) {
+				--state->source_storage;
+				state->source_index = state->storages[state->source_storage].usage - 1;
+			}
+			break;
+			case sk_Up:
+			if (state->source_index < state->storages[state->source_storage].usage - 1) {
+				++state->source_index;
+			}
+			break;
+			case sk_Down:
+			if (state->source_index > 0) {
+				--state->source_index;
+			}
+			break;
+			case sk_Enter:
+			if (state->source_index != 255) {
+				state->target_storage = state->source_storage;
+				state->gamestate = 1;
+			}
+			break;
+		}
+	}
+	else if (state->gamestate == 1) {
+		switch (key) {
+			case sk_Right:
+			if (state->target_storage < 12) {
+				++state->target_storage;
+			}
+			break;
+			case sk_Left:
+			if (state->target_storage > 0) {
+				--state->target_storage;
+			}
+			break;
+			case sk_Enter:
+			if (state->source_index != 255) {
+				Card card = CardStorage_take_card(&state->storages[state->source_storage], state->source_index);
+				CardStorage_add_card(&state->storages[state->target_storage], card);
+				state->gamestate = 0;
+				state->source_storage = 0;
+				state->source_index = 0;
+				state->target_storage = 0;
+			}
+			break;
+		}
+	}
+    return key;
 }
 
-int drawCard(Card card, int x, int y) {
+int drawCard(Card card, int x, int y, bool highlight) {
 	if ((card & CARD_FACING_MASK) == CARD_FACEDOWN) {
 		gfx_TransparentSprite(bard_back, x, y);
-		return 0;
 	}
-	gfx_TransparentSprite(blank_card, x, y);
-	int glif1_x = x + 2;
-	int glif1_y = y + 2;
-	int glif2_x = x + 32; // 40 - 2 - 6
-	int glif2_y = y + 44; // 56 - 10 - 2
-	if ((card & CARD_VALUE_MASK) == CARD_10)
-		glif2_x -= 2;
-	if ((card & CARD_VALUE_MASK) == CARD_Q)
-		glif2_x -= 1;
-	gfx_TransparentSprite(glifs[card], glif1_x, glif1_y);
-	gfx_TransparentSprite(glifs_flipped[card], glif2_x, glif2_y);
-	dbg_printf("card value: %d, card suit: %d, full card: %d\n", ((u24)card & CARD_VALUE_MASK) >> 2, (u24)card & 0b11, (u24)card);
+	else {
+		gfx_TransparentSprite(blank_card, x, y);
+		int glif1_x = x + 2;
+		int glif1_y = y + 2;
+		int glif2_x = x + 32; // 40 - 2 - 6
+		int glif2_y = y + 44; // 56 - 10 - 2
+		if ((card & CARD_VALUE_MASK) == CARD_10)
+			glif2_x -= 2;
+		if ((card & CARD_VALUE_MASK) == CARD_Q)
+			glif2_x -= 1;
+		gfx_TransparentSprite(glifs[card], glif1_x, glif1_y);
+		gfx_TransparentSprite(glifs_flipped[card], glif2_x, glif2_y);
+		dbg_printf("card value: %d, card suit: %d, full card: %d\n", ((u24)card & CARD_VALUE_MASK) >> 2, (u24)card & 0b11, (u24)card);
+	}
+	if (highlight) {
+		gfx_SetTextScale(1, 1);
+		gfx_PrintStringXY("Selected", x, y);
+	}
 	return 0;
 }
 
@@ -329,10 +424,35 @@ int drawCard(Card card, int x, int y) {
 void draw(State* state)
 {
 	gfx_ZeroScreen();
+	// playfield
 	for (u8 colum = 0; colum < 7; ++colum) {
-		for (u8 j = 0; j < state->colums[colum].usage; ++j) {
-			drawCard(state->colums[colum].data[j], colum * 46, j * 17 + 60);
+		for (u8 j = 0; j < state->storages[COLUM + colum].usage; ++j) {
+			drawCard(
+				state->storages[COLUM + colum].data[j],
+				colum * 46, j * 17 + 60,
+				(COLUM + colum) == state->source_storage && j == state->source_index);
 		}
 	}
+
+	// deck and discard
+	u8 num_to_draw = min(3, state->storages[DECK].usage);
+	for (u8 i = 0; i < num_to_draw; ++i) {
+		drawCard(state->storages[DECK].data[state->storages[DECK].usage - 1 - i], 2 + i * 16, 2, false);
+	}
+	num_to_draw = min(3, state->storages[DISCARD].usage);
+	for (u8 i = 0; i < num_to_draw; ++i) {
+		drawCard(state->storages[DISCARD].data[state->storages[DECK].usage - 1 - i], 80 + i * 16, 2, false);
+	}
+
+	gfx_SetTextScale(2, 2);
+	gfx_PrintStringXY("sel_sto:", 0, 192);
+	gfx_PrintInt(state->source_storage, 2);
+	gfx_PrintStringXY("sel_index:", 0, 208);
+	gfx_PrintInt(state->source_index, 2);
+	gfx_PrintStringXY("tar_sto:", 0, 224);
+	gfx_PrintInt(state->target_storage, 2);
+	gfx_PrintString(" state:");
+	gfx_PrintInt(state->gamestate, 2);
+
 }
 
